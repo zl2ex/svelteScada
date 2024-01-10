ae930022af00cb2d0c79cc691a4df691794f2134
tasmanLinuxMint <zl2ex@outlook.com>
2024-01-10 22:05:35 +1300

    almost reverted back to inital haha

--- server/socketIoHandler.ts
+++ server/socketIoHandler.ts
@@ -17,7 +17,6 @@ export var server = {
 
 
     injectSocketIO: function (server) {
-    
         const io = new Server(server, {
             cors: {
                 origin: "http://localhost"
@@ -25,18 +24,55 @@ export var server = {
         });
     
     
-        this._tags = getSetIfy(tagsInit, "tags",
-            (path: string, value: any) => {
+        /*this._tags = getSetIfy({
+            obj: tagsInit,
+            path: "tags",
+            getCb: (path: string, value: any) => {
                 return value;
             },
-            (path: string, value: any, newValue: any) => {
+            setCb: (path: string, value: any, newValue: any) => {
                 console.log('setting', path, newValue);
                 value = newValue;
                 io.emit(`${path}:update`, value);
                 return value;
-            },
-            () => {}
-        );
+            }
+        });*/
+
+        function socketIoIfy(obj: any, path: string): any
+        {
+            if(typeof obj === 'object')
+            {
+                let output = {};
+                for(let key in obj) 
+                {
+                    let path_key = `${path}.${key}`;
+                    if(typeof obj[key] === 'object') 
+                    {
+                        obj[key] = socketIoIfy(obj[key], path_key);
+                    }
+                    console.log(`define properties ${path_key}`);
+                    Object.defineProperty(output, key, {
+                        get() { return obj[key]; },
+                        set(newVal)
+                        {
+                            console.log('setting', path, newVal);
+                            io.emit(`${path}:update`, newVal);
+                            obj[key] = newVal;
+                        },
+                        enumerable: true
+                    });
+                }
+                return output;
+            } 
+            else
+            {
+                console.error("please provide an object    this is not an object :", obj);
+                return obj;
+            }
+        }
+
+        this._tags = socketIoIfy(tagsInit, "tags");
+
 
         io.on("connection", (socket) => {
     
@@ -46,16 +82,44 @@ export var server = {
                 console.log("socket disconnected  id " + socket.id);
             });
 
-            getSetIfy(this._tags, "tags",
-                () => {},
-                () => {},
-                (path: string, value: any) => {
+            function registerUpdateEvents(obj: any, path: string): any
+            {
+                if(typeof obj === 'object')
+                {
+                    for(let key in obj) 
+                    {
+                        let path_key = `${path}.${key}`;
+                        if(typeof obj[key] === 'object') 
+                        {
+                            registerUpdateEvents(obj[key], path_key);
+                        }
+                        console.log(`register update events ${path_key}`);
+                        
+                        socket.on(`${path}:update`, (value) => {
+                            socket.broadcast.emit(`${path}:update`, value);
+                            console.log(`${path}:update`, value);
+                            obj[key] = value;
+                        });
+                    }
+                } 
+                else
+                {
+                    console.error("please provide an object    this is not an object :", obj);
+                }
+            }
+
+            registerUpdateEvents(this._tags, "tags");
+
+            /*getSetIfy({
+                obj: this._tags, 
+                path: "tags",
+                initCb: (path: string, value: any) => {
                     socket.on(`${path}:update`, (newValue) => {
                         socket.broadcast.emit(`${path}:update`, newValue);
                         console.log(`${path}:update`, newValue);
                         value = newValue;
                     });
-            });
+            }});*/
         });
         
         console.log('SocketIO Tag server running  Tags\n', this._tags);
--- src/lib/tag/getSetIfy.ts
+++ src/lib/tag/getSetIfy.ts
@@ -1,45 +1,47 @@
 
+type getSetIfyProps = {
+	obj: any;
+	path: string;
+	getCb?: Function;
+	setCb?: Function;
+	initCb?: Function;
+}
+
+
+
 // pass any object or array and specify events to run when properties are accessed
-export function getSetIfy(obj: any, path: string, getCb: Function, setCb: Function, initCb: Function): any
+export function getSetIfy(p: getSetIfyProps): any
 {
-	/*if(Array.isArray(obj)) 
-	{
-		let items = [];
-		for (let i = 0; i < obj.length; i++) 
-		{
-			console.log(`define items ${path}[${i}]`);
-			items[i] = getSetIfy(obj[i], `${path}[${i}]`, getCb, setCb, initCb);
-		}
-		return items;
-	} 
-	else */if(typeof obj === 'object')
+	if(typeof p.obj === 'object')
 	{
 		let output = {};
-		for(let key in obj) 
+		for(let key in p.obj) 
 		{
-			let path_key = `${path}.${key}`;
-			if(typeof obj[key] === 'object') 
+			let path_key = `${p.path}.${key}`;
+			if(typeof p.obj[key] === 'object') 
 			{
-				obj[key] = getSetIfy(obj[key], path_key, getCb, setCb, initCb);
+				p.obj[key] = getSetIfy({obj: p.obj[key], path: path_key, setCb: p.getCb, getCb: p.setCb, initCb: p.initCb});
 			}
 			console.log(`define properties ${path_key}`);
-			Object.defineProperty(output, key, {
-				get() { return getCb(path_key, obj[key]); },
-				set(newVal)
-				{
-                    obj[key] = setCb(path_key, obj[key], newVal);
-				},
-				enumerable: true
-			});
-
-			initCb(path_key, obj[key]);
+			if(p.initCb) p.initCb(path_key, p.obj[key]);
+			if(p.getCb && p.setCb)
+			{
+				Object.defineProperty(output, key, {
+					get() { return p.getCb(path_key, p.obj[key]); },
+					set(newVal)
+					{
+						p.obj[key] = p.setCb(path_key, p.obj[key], newVal);
+					},
+					enumerable: true
+				});
+			}
 		}
 		return output;
 	} 
 	else
 	{
 		console.error("please provide an object    this is not an object :", obj);
-		return obj;
+		return p.obj;
 	}
 }
 
